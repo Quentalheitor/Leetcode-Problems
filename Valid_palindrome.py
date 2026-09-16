29967f8ff2d45df4f27d0d7acc41000afced3bc8
Heitor Quental <heitorquental321@gmail.com>
2026-09-16 11:51:22 -0300

    Valid_palindrome 16/09/26

---
+++ Valid_palindrome.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ("".join(x for x in s if x.isalnum())).lower()
+        if s == s[::-1]:
+            return True
+        return False
+
+
+solucao = Solution
+solucao.isPalindrome(solucao,s="A man, a plan, a canal: Panama")
